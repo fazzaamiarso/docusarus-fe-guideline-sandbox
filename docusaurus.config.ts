5d438ad95ba09fc4db54419e3e819a833265ff79
fazzaamiarso <fazzaamiarso@gmail.com>
2025-02-02 16:26:18 +0700

    fix: change base route

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docusarus-fe-guideline-sandbox",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
