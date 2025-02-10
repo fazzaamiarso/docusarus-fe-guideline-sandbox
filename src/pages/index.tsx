ccdfaaaa60e421a64caf730ebecccba25e27749f
fazzaamiarso <fazzaamiarso@gmail.com>
2025-02-10 15:07:29 +0700

    chore: redirect from home page to guide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,7 @@
 import type { ReactNode } from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-
-import styles from "./index.module.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-      </div>
-    </header>
-  );
-}
+import { Redirect } from "@docusaurus/router";
 
 export default function Home(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main></main>
-    </Layout>
-  );
+  // go straight to the guide instead of home page
+  return <Redirect to="code-editor-setup" />;
 }
